7215cad676d69e5f89651729771c48cbe1dd3561
danielssTech <danielss.tech195@gmail.com>
2026-04-21 15:42:59 +0100

    adding last important endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,51 @@
 from fastapi import FastAPI
 import db
-
+import vigenere
 app = FastAPI()
 
 
+
+
+#social-media
 @app.get("/users")
 def get_users():
     data = db.read_json()
     return data["users"]
 
-
 @app.get("/comments")
-def get_users():
+def get_comments():
     data = db.read_json()
     return data["comments"]
+
+@app.post("/comment")
+def create_comment_encrypt (comment: str, key: str): 
+    data = db.read_json()
+    encrypt_message = vigenere.vigenere_encode(comment, key)
+    new_id = len(data["comments"]) + 1
+    comment_key = f"comment{new_id}"
+    new_comment = {
+        "id": str(new_id),
+        "plain_text": comment, 
+        "encrypt_text":encrypt_message
+    }
+
+    data["comments"][comment_key] = new_comment
+
+    db.write_json(data)
+    
+    return "ok"
+
+@app.post("/decrypt")
+def decrypt_comment (key: str, comment_id: str):
+    data = db.read_json()
+    
+    for comment in data["comments"].values():
+        if comment["id"] == comment_id:
+            comment_crypt = comment["encrypt_text"]
+            comment_decrypt = vigenere.vigenere_decode (comment_crypt, key)
+            return comment_decrypt
+                    
+    return "not found"
+
+
 
