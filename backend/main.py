1702ccc7a0b6220fcbed389d4cd7a7f4dab560bd
bram <bram.de.craene@student.howest.be>
2026-04-23 10:06:18 +0100

    commit for pull thu morging

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import db
+import vigenere
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/users")
 def get_users():
@@ -15,3 +23,12 @@ def get_users():
     data = db.read_json()
     return data["comments"]
 
+@app.get("/txt2vigenere/{text}/{key}")
+def txt2vigenere(text: str, key: str):
+    encoded_text = vigenere.vigenere_encode(text, key)
+    return {"encoded_text": encoded_text}
+
+@app.get("/vigenere2txt/{ciphertext}/{key}")
+def vigenere2txt(ciphertext: str, key: str):
+    decoded_text = vigenere.vigenere_decode(ciphertext, key)
+    return {"decoded_text": decoded_text}
